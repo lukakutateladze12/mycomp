8597641c1c26ec7108217fd05e20e5b3a53aaf01
lukakutateladze12 <90317365+lukakutateladze12@users.noreply.github.com>
2023-04-06 16:10:07 +0400

    This PC

---
+++ app.js
@@ -0,0 +1,9 @@
+let myComputerManufacturer = "Lenovo IdeaPad 3 ";
+let myComputerMemory = "8GB ";
+let myComputerOS = "Windows";
+let myComputerCPU = "Intel Core I5 CPU, 11th Gen";
+const computerBuyingYear = 2022;
+let currentYear = 2023;
+let durration = currentYear - computerBuyingYear;
+let sentence = 'Hello, My computer is ' + myComputerManufacturer + ', it turns on ' + myComputerOS + " and I've been using it for " + durration + " year. It also has " + myComputerCPU 
+console.log(sentence );
